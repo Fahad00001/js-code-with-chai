3e113f6e58ee1cb704814e6f2f393f3128c03e1d
Fahad <145900785+Fahad00001@users.noreply.github.com>
2025-04-10 07:24:07 +0000

    summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,22 @@
+let myyoutubename="fahaad@gmail.com"
+let anothername=myyoutubename
+anothername="chaiaurcode"
+
+
+console.log(myyoutubename);
+console.log(anothername);
+
+let userone={
+    email:"user@google.com",
+    upi:"user@ybl"
+}
+let usertwo=userone
+usertwo.email="fahad@gmail.com"
+console.log(userone.email);
+console.log(usertwo.email);
+
+
+
+
+
+
