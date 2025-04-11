09be13851482f1ed190fdd8488444c173fed9e7a
Fahad <145900785+Fahad00001@users.noreply.github.com>
2025-04-11 05:48:19 +0000

    string

---
+++ 01_basics/string.js
@@ -0,0 +1,22 @@
+const gameName=new String('fahad-hc-iqbal')
+console.log(gameName[0]);
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('d'));
+const newstringOne="   hitesh     "
+console.log(newstringOne);
+console.log(newstringOne.trim());
+const url=("https://fahad.com/fahad%20iqbal");
+console.log(url.replace('%20','-'));
+console.log(url.includes('sundar'));
+console.log(gameName.split('-'));
+
+const hello='Hello'
+console.log(hello.concat(' kevin ',' have a nice day'));
+
+
+
+
+
+
+
+
