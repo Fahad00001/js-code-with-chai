e0a983d9b7416801e0a121612930f87a4bfac2af
Fahad <145900785+Fahad00001@users.noreply.github.com>
2025-04-11 14:23:50 +0000

    nums

---
+++ 01_basics/nums.js
@@ -0,0 +1,30 @@
+// const score=400
+// console.log(score);
+
+// const balance=new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+
+
+// const othernumber=123.4231
+// console.log(othernumber.toPrecision(3));
+// const hundered=10000000
+// console.log(hundered.toLocaleString('en-IN'));
+
+
+// maths 
+
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
