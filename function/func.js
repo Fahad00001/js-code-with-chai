2b80a389cd32ddeec52f01987932f1b5336381c1
Fahad <145900785+Fahad00001@users.noreply.github.com>
2025-04-13 07:12:23 +0000

    func

---
+++ function/func.js
@@ -0,0 +1,15 @@
+function addtwonumber(num1,num2){
+ console.log( num1+num2);
+}
+
+addtwonumber(3,5)
+
+function calculatecrtprice(val1,val2,...num1){
+    return num1
+}
+// console.log(val1);
+// console.log(val2);
+
+
+console.log(calculatecrtprice(200,400,5000));
+
